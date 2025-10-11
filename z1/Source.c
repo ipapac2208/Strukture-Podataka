671e550d914afeb233723bfb1fbfe3e423d23f3d
ipapac2208 <nixa>
2025-10-11 13:28:49 +0200

    ver 3

--- z1/Source.c
+++ z1/Source.c
@@ -1,4 +1,4 @@
-#define CRT_SECURE_NO_WARNINGS_
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,23 +6,61 @@ typedef struct student {
 	char ime[50];
 	char prezime[50];
 	int brojBodova;
+	double postotak;
 } stud;
 
-int brojStud(){
+int brojStud() {
 
-	FILE* fp;
-	fp = fopen("Studenti.txt", "r");
+	FILE* fp = fopen("Studenti.txt", "r");
+	int brojac = 1;
+	char c;
 
-	if (fp == NULL) {
+
+	if (!fp) {						//provjera jeli file postoji
 		printf("GRESKA: file ne postoji");
+		return -1;
 	}
 	else {
-		while (!feof(fp)) {
-		
+		while (!feof(fp)) {					//brojanje studenata
+			c = fgetc(fp);
+			if (c == '\n')
+				brojac++;
 		}
 	}
+	fclose(fp);
+	return brojac;
 }
+
+stud* studPodatci(int br) {
+	stud* student;
+	student = (stud*)malloc(br * sizeof(stud));  //alokacija memorije
+	int i = 0;
+	FILE* fp1 = fopen("Studenti.txt", "r");
+
+	if (!student) {
+		printf("GRESKA: Nema studenata");
+		return NULL;
+	}
+	while (!feof(fp1)) {						//zapisivanje u strukturu student
+		fscanf(fp1, "%s %s %d", student[i].ime, student[i].prezime, &student[i].brojBodova);
+		student[i].postotak = (double)student[i].brojBodova / 100;
+		i++;
+	}
+	fclose(fp1);
+	return student;
+}
+
 
 int main() {
 
+	stud* student;
+	int br = brojStud();
+
+	printf("Broj studenata: %d\n", br);
+	student = studPodatci(br);
+	printf("podatci o studentima:\n");
+	for (int i = 0; i < br; i++)
+		printf("ime: %s\nprezime: %s\nbroj bodova: %d\npostotak: %lf\n", student[i].ime, student[i].prezime, student[i].brojBodova, student[i].postotak*100);
+
+	return 0;
 }
