01a813d796b82a9b77b1651355191df071655fb3
ipapac2208 <nixa>
2025-12-04 14:12:46 +0100

    sada radi normalno ali ne radi print

--- z4/Source.c
+++ z4/Source.c
@@ -15,75 +15,79 @@ typedef struct polynomial {
 int readFile(pos, pos);
 int insertPolynomial(pos, int, int);
 int printPolynomial(pos);
-int sumPolynomial(pos);
-int mpyPolynomial(pos);
+pos sumPolynomial(pos);
+int mpyPolynomial(pos, pos);
 int deleteAll(pos);
 
 
 int main() {
 	Polynomial head = { 0,0,NULL };
 	Polynomial head1 = { 0,0,NULL };
+	
 	if (readFile(&head, &head1) != NOT_FOUND) {
-		sumPolynomial(head.next, head1.next);
-		mpyPolynomial(head.next);
-	}
+	 sumPolynomial(head.next, head1.next);
 	deleteAll(head.next);
 	deleteAll(head1.next);
+	}
+	if (readFile(&head, &head1) != NOT_FOUND) {
+		mpyPolynomial(head.next, head1.next);
+		deleteAll(head.next);
+		deleteAll(head1.next);
+	}
 
 	return 0;
 }
 
 int deleteAll(pos p) {
+	pos temp = p;
 	pos q;
-	while (p->next != NULL)
+	while (temp->next != NULL)
 	{
-		q = p->next;
-		p->next = q->next;
+		q = temp->next;
+		temp->next = q->next;
 		q->next = NULL;
 		free(q);
 	}
-	p->next = NULL;
-	free(p);
+	temp->next = NULL;
+	
 	return 0;
 }
 
-int mpyPolynomial(pos p) {
-	pos q = p->next;
-	pos result = (pos)malloc(sizeof(Polynomial));
+int mpyPolynomial(pos p, pos q) {
+	pos result = p;
 	
 	if (!result) return STRUCT_ERROR;
 
-	result = p;
-	while (q != NULL) {
-		result->number *= q->number;
-		result->power += q->power;
+	while (p != NULL && q != NULL && result != NULL) {
+		result->number = p->number * q->number;
+		result->power = p->power + q->power;
+		p = p->next;
 		q = q->next;
+		result = result->next;
 	}
+	printf("Umnozak:\n");
 	printPolynomial(result);
-	deleteAll(result);
 	return 0;
 }
 
-int sumPolynomial(pos p, pos q) {
+pos sumPolynomial(pos p, pos q) {
 	Polynomial result = { 0,0,NULL };
 	pos res = &result;
 	
 	for (pos temp = p; temp != NULL; temp = temp->next)
 		insertPolynomial(res, temp->number, temp->power);
 
-	for (pos temp1 = p; temp1 != NULL; temp1 = temp1->next)
+	for (pos temp1 = q; temp1 != NULL; temp1 = temp1->next)
 		insertPolynomial(res, temp1->number, temp1->power);
-
+	printf("Zbroj:\n");
 	printPolynomial(res->next);
-	deleteAll(res->next);
-
-	return 0;
+	return res;
 }
 
 int printPolynomial(pos p) {
 	while (p != NULL)
 	{
-		printf("%d * x ^ %d\n", p->number, p->power);
+		printf("%d * x ^ %d \n", p->number, p->power);
 		p = p->next;
 	}
 	return 0;
@@ -100,20 +104,21 @@ int readFile(pos p, pos q) {
 
 	if (!fp || !fp1)
 		return NOT_FOUND;
-
+	
 	while(!feof(fp)&& !feof(fp1)){
 		fscanf(fp, "%d", &tmpnum);
 		fscanf(fp1, "%d", &tmppow);
 		insertPolynomial(p, tmpnum, tmppow);
 	}
-
+	
 	while (!feof(fp2) && !feof(fp3)) {
 		fscanf(fp2, "%d", &tmpnum);
 		fscanf(fp3, "%d", &tmppow);
 		insertPolynomial(q, tmpnum, tmppow);
 	}
-
+	printf("Prvi polinom:\n");
 	printPolynomial(p->next);
+	printf("drugi polinom:\n");
 	printPolynomial(q->next);
 	return 0;
 }
@@ -127,13 +132,24 @@ int insertPolynomial(pos p, int tempnum, int temppow) {
 	q->number = tempnum;
 	q->power = temppow;
 
+	pos curr = p;
 
-	while (p->next != NULL && p->power > q->power) {
-		p = p->next;
-	}
-	if (p->next != NULL && p->power == q->power) {
-		p->number += q->number;
-		free(q);
+	if (p->next != NULL) {
+		curr = p->next;
+
+		while (curr->next != NULL && curr->power < q->power) {
+			curr = curr->next;
+		}
+		if (curr != NULL) {
+			if (curr->power == q->power) {
+				curr->number = curr->number + tempnum;
+				free(q);
+			}
+			else {
+				q->next = curr->next;
+				curr->next = q;
+			}
+		}
 	}
 	else {
 		q->next = p->next;
